50518df67d2ccd66d803e224008ce1d5ab7915e9
Sheriff  Oladimeji <dimejiademola5@gmail.com>
2024-08-04 09:52:15 +0100

    fix: add type for quote

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import Image from "next/image";
 
+interface Quote {
+  _id: string;
+  quote: string;
+  author: string;
+}
+
 async function getData() {
   const res = await fetch('https://myst-api.onrender.com/api/v1/quotes')
   if (!res.ok) {
@@ -15,7 +21,7 @@ export default  async function Home() {
 
   return (
     <main className="w-[90%] mx-auto pt-24 grid md:grid-cols-3 gap-4">
-     {quotes.map((data) => (
+     {quotes.map((data: Quote) => (
        <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700" key={data._id}> 
        <p>{data.quote}</p>   
        <p>{data.author}</p>
