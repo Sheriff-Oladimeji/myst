9c893d0aa403b0768dc708d35d025f2ee2b43332
Sheriff  Oladimeji <dimejiademola5@gmail.com>
2024-08-05 13:01:35 +0100

    feat: add copy to clipboard

--- src/components/QuoteCard.tsx
+++ src/components/QuoteCard.tsx
@@ -1,11 +1,28 @@
+"use client"
 import { Quote } from "@/types/quote";
 import React from "react";
+import { IoMdCopy } from "react-icons/io";
+import { PiHeartThin } from "react-icons/pi";
 
-const QuoteCard = ({ quote, author , _id}: Quote) => {
+const QuoteCard = ({ quote, author, _id }: Quote) => {
   return (
-    <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-      <p>{quote}</p>
-      <p>{author}</p>
+    <div className="max-w-md p-6 bg-white border border-gray-200 rounded-md shadow  ">
+      <div className="flex flex-col gap-3">
+        <p>{quote}</p>
+        <p className="text-sm text-blue-700">{author}</p>
+      </div>
+      <div className="flex justify-between items-center border-t border-gray-200  mt-2 pt-2">
+        <div>
+          <PiHeartThin size={20} />
+        </div>
+        <button
+          onClick={() => {
+            navigator.clipboard.writeText(quote);
+          }}
+        >
+          <IoMdCopy size={20} />
+        </button>
+      </div>
     </div>
   );
 };
