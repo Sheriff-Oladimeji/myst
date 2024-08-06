7678b20f8d5fa60e37eec872b5bada07f8153eb1
Sheriff  Oladimeji <dimejiademola5@gmail.com>
2024-08-06 15:21:19 +0100

    feat: update add quote page

--- src/app/new-quote/page.tsx
+++ src/app/new-quote/page.tsx
@@ -38,24 +38,47 @@ const AddQuote = () => {
   };
 
   return (
-    <form onSubmit={onSubmit} className="flex flex-col gap-4">
-      <input
-        type="text"
-        name="quote"
-        placeholder="Enter quote"
-        value={quote}
-        onChange={(e) => setQuote(e.target.value)}
-        required
-      />
-      <input
-        type="text"
-        name="author"
-        placeholder="Enter author"
-        value={author}
-        onChange={(e) => setAuthor(e.target.value)}
-        required
-      />
-      <button type="submit" disabled={isLoading}>
+    <form onSubmit={onSubmit} className="flex flex-col gap-4 max-w-sm mx-auto">
+      <div>
+        <label
+          htmlFor="quote"
+          className="block mb-2 text-sm font-medium text-white"
+        >
+          Quote
+        </label>
+        <input
+          type="text"
+          name="quote"
+          placeholder="Enter quote"
+          className="block w-full p-6 text-gray-900 border border-gray-300  bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 rounded-xl"
+          value={quote}
+          onChange={(e) => setQuote(e.target.value)}
+          required
+        />
+      </div>
+      <div>
+        <label
+          htmlFor="author"
+          className="block mb-2 text-sm font-medium text-white"
+        >
+          Quote Author
+        </label>
+
+        <input
+          type="text"
+          name="author"
+          placeholder="Enter author"
+          className="block w-full p-3 text-gray-900 border border-gray-300  bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 rounded-xl"
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+          required
+        />
+      </div>
+      <button
+        type="submit"
+        disabled={isLoading}
+        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-[7px] text-sm px-5 py-2.5 text-center w-max"
+      >
         {isLoading ? "Loading..." : "Submit"}
       </button>
     </form>
