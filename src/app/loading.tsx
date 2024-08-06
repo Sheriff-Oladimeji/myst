e775a60dee23bf1010e1cd9571ee1633c45a793d
Sheriff  Oladimeji <dimejiademola5@gmail.com>
2024-08-06 14:58:47 +0100

    fix: update loader

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 const loading = () => {
   return (
-    <div className='flex flex-col h-screen justify-center items-center'>loading...</div>
-  )
+    <div className="flex flex-col h-screen justify-center items-center">
+      <div className="relative inline-flex">
+        <div className="w-8 h-8 bg-blue-500 rounded-full"></div>
+        <div className="w-8 h-8 bg-blue-500 rounded-full absolute top-0 left-0 animate-ping"></div>
+        <div className="w-8 h-8 bg-blue-500 rounded-full absolute top-0 left-0 animate-pulse"></div>
+      </div>
+    </div>
+  );
 }
 
 export default loading
