17c61f550e6d5cbb17fec43ba389af6a3271dcbb
Sheriff  Oladimeji <dimejiademola5@gmail.com>
2024-08-06 07:14:37 +0100

    feat: add loading to homepage

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const loading = () => {
+  return (
+    <div className='flex flex-col h-screen justify-between items-center'>loading...</div>
+  )
+}
+
+export default loading
