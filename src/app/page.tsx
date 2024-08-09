62940e4394f4d51098d519ff1c1874c39cff6a44
Sheriff  Oladimeji <dimejiademola5@gmail.com>
2024-08-09 07:21:23 +0100

    fix: add runtime to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 import AllQuotes from "@/components/AllQuotes";
 import QOD from "@/components/QOD";
-
+  export const runtime = "edge";
 export default function Home() {
   return (
     <main >
