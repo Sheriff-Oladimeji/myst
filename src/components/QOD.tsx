037a381dd95d5a54d0279d283c41c7237f256de4
Sheriff  Oladimeji <dimejiademola5@gmail.com>
2024-08-13 14:15:08 +0100

    fix[style]: restyle quote of the day

--- src/components/QOD.tsx
+++ src/components/QOD.tsx
@@ -18,8 +18,8 @@ const QOD = () => {
 
   if (isLoading)
     return (
-      <p className="text-white text-lg flex items-center justify-center text-center">
-        Loading Quote...
+      <p className="text-white text-sm flex items-center justify-center text-center">
+        Loading Quote of the day...
       </p>
     );
   if (error)
@@ -37,13 +37,15 @@ const QOD = () => {
           'url("https://images.unsplash.com/photo-1611757644023-192a39fc9359?q=80&w=2874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
       }}
     >
-      <div className="bg-black bg-opacity-50 p-6 md:p-4 rounded-lg max-w-xl sm:max-w-2xl">
-        <h1 className="text-3xl font-bold mb-3">Quote of the Day</h1>
-        <p className="text-xl md:text-lg italic mb-4">
-          &quot;{data.quote}&quot;
-        </p>
-        <p className="text-lg md:text-md font-semibold">- {data.author}</p>
-        <p className="text-md md:text-sm text-gray-400">{data.category}</p>
+      <div className="bg-black bg-opacity-50 p-6 md:p-4  h-full w-full">
+        <div className="max-w-xl sm:max-w-2xl flex flex-col justify-center items-center h-full text-center mx-auto">
+          <h1 className="text-3xl font-bold mb-3">Quote of the Day</h1>
+          <p className="text-xl md:text-lg italic mb-4">
+            &quot;{data.quote}&quot;
+          </p>
+          <p className="text-lg md:text-md font-semibold">- {data.author}</p>
+          <p className="text-md md:text-sm text-gray-400">{data.category}</p>
+        </div>
       </div>
     </header>
   );
