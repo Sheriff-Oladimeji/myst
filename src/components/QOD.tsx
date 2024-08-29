c8e74073029dc8947fdfebb0f9b20055f5b5a3a4
Sheriff  Oladimeji <dimejiademola5@gmail.com>
2024-08-29 09:07:16 +0100

    fix: reduce load time for qod

--- src/components/QOD.tsx
+++ src/components/QOD.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import { useQuery } from "@tanstack/react-query";
+import Image from "next/image";
 
 const QOD = () => {
   const { isLoading, error, data } = useQuery({
@@ -12,6 +13,7 @@ const QOD = () => {
         }
         return res.json();
       }),
+    staleTime: 1000 * 60 * 60 * 24, // 24 hours
   });
 
   if (isLoading)
@@ -28,14 +30,16 @@ const QOD = () => {
     );
 
   return (
-    <header
-      className="relative h-96 md:h-80 flex items-center justify-center text-center text-white bg-cover bg-center m-0"
-      style={{
-        backgroundImage:
-          'url("https://images.unsplash.com/photo-1611757644023-192a39fc9359?q=80&w=2874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
-      }}
-    >
-      <div className="bg-black bg-opacity-40 p-6 md:p-4  h-full w-full">
+    <header className="relative h-96 md:h-80 flex items-center justify-center text-center text-white m-0">
+      <Image
+        src="https://images.unsplash.com/photo-1611757644023-192a39fc9359?q=80&w=1200&auto=format&fit=crop"
+        alt="Background"
+        layout="fill"
+        objectFit="cover"
+        quality={75}
+        priority
+      />
+      <div className="absolute inset-0 bg-black bg-opacity-40 p-6 md:p-4">
         <div className="max-w-xl sm:max-w-2xl flex flex-col justify-center items-center h-full text-center mx-auto">
           <h1 className="text-3xl font-bold mb-3">Quote of the Day</h1>
           <p className="text-xl md:text-lg italic mb-4">
